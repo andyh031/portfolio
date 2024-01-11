d00b12ba818e57632c6311eb14e2815ddf62d653
Andy Hu <andy45.hu@gmail.com>
2024-01-11 10:47:11 -0800

    fix delay

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -115,6 +115,7 @@ export default function Experience({ setBkgColor }) {
                   borderRadius="50%"
                   objectFit="cover"
                   src={gradImage}
+                  onLoad={handleImageLoaded}
                 />
               </motion.div>
             </Flex>
